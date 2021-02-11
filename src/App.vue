2175bc3f2505f94de4452bf5ef2ca75e623782ef
Alvaro Saburido <alvaro.saburido@gmail.com>
2021-02-11 18:36:46 +0100

    clean main

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,11 @@
-<template>
-  <div class="page p-8">
-    <button
-      type="button"
-      @click="showModal = !showModal"
-      class="mx-auto w-full flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-    >
-      Open Modal
-    </button>
-    <ModalDialog :show="showModal">
-      <p class="mb-4">Gokuu is...</p>
-      <img src="https://i.gifer.com/QjMQ.gif" />
-    </ModalDialog>
-  </div>
-</template>
+<template>App</template>
 
 <script>
-import ModalDialog from './components/ModalDialog.vue';
-import { ref } from 'vue';
-
-const components = {
-  ModalDialog,
-};
+const components = {};
 export default {
   name: 'App',
   components,
-  setup() {
-    const showModal = ref(false);
-    return {
-      showModal,
-    };
-  },
+  setup() {},
 };
 </script>
 
